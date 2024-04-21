b824283e612de3fc3cb44b9bd310fede4bb81381
João <joaoggponciano@gmail.com>
2024-04-21 17:18:44 -0300

    Add dark mode and src avatar

--- controller/View1.controller.js
+++ controller/View1.controller.js
@@ -10,6 +10,13 @@ sap.ui.define([
         return Controller.extend("myportifolioapp.controller.View1", {
             onInit: function () {
 
+            },
+
+            onChangeTheme: function(oEvent) {   
+                const sIcon = oEvent.getSource().getIcon();
+
+                sap.ui.getCore().getConfiguration().setTheme(sIcon.includes("dark") ? "sap_horizon_dark" : "sap_horizon" );
+                oEvent.getSource().setIcon(sIcon.includes("dark") ? "sap-icon://light-mode" : "sap-icon://dark-mode")
             }
         });
     });
